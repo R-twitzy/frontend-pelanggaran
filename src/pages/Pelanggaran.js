446e57cad792e49f08f92ee5bd086624960f810f
R-twitzy <ramadhanmaulana446@gmail.com>
2022-04-12 10:24:42 +0700

    update 12 Apr 2022

--- src/pages/Pelanggaran.js
+++ src/pages/Pelanggaran.js
@@ -1,7 +1,18 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Toast } from "bootstrap";
+import { Modal } from "bootstrap";
+
 export default function Pelanggaran() {
     let [pelanggaran, setPelanggaran] = useState([])
+    let [message, setMessage] = useState("")
+
+    let [idPelanggaran, setIdPelanggaran] = useState(0)
+    let [namaPelanggaran, setNamaPelanggaran] = useState("")
+    let [poin, setPoin] = useState(0)
+    let [action, setAction] = useState("")
+
+    let [modal, setModal] = useState(null)
 
     // get token from localstorage
     let token = localStorage.getItem(`token-pelanggaran`)
@@ -10,6 +21,21 @@ export default function Pelanggaran() {
         headers: {
             Authorization: `Bearer ${token}`
         }
+    }
+
+    // create a function to show a toast
+    let showToast = message => {
+        let myToast = new Toast(
+            document.getElementById(`myToast`),
+            {
+                autohide: true
+            }
+        )
+        // perintah utk mengisi state 'message'
+        setMessage(message)
+
+        // show the toast
+        myToast.show()
     }
 
     //create function to get data pelanggaran from backend
@@ -27,16 +53,83 @@ export default function Pelanggaran() {
             .then(response => {
                 // simpan di state pelanggaran
                 setPelanggaran(response.data)
+
+                // call showtoast
+                showToast(`Data Pelanggaran berhasil dimuat`)
             })
             .catch(error => console.log(error))
     }
 
+    let tambahData = () => {
+        // show modal
+        modal.show()
+
+        // mengosongkan inputan form
+        setIdPelanggaran(0)
+        setNamaPelanggaran("")
+        setPoin(0)
+        setAction('insert')
+    }
+
+    let editData = item => {
+        // show modal
+        modal.show()
+
+        // mengisi form sesuai data yg dipilih
+        setIdPelanggaran(item.id_pelanggaran)
+        setNamaPelanggaran(item.nama_pelanggaran)
+        setPoin(item.poin)
+        setAction('edit')
+    }
+
+    let saveData = event => {
+        event.preventDefault()
+        /** close modal */
+        modal.hide()
+        if (action === 'insert') {
+            let endpoint = `http://localhost:8080/pelanggaran`
+            let request = {
+                nama_pelanggaran: namaPelanggaran,
+                poin: poin
+            }
+
+            /** send data */
+            axios.post(endpoint, request, authorization)
+            .then(response => {
+                showToast(response.data.message)
+                /** refresh data pelanggaran */
+                getData()
+            })
+            .catch(error => console.log(error))
+        } else if (action === 'edit') {
+            
+        }
+    }
+
     useEffect(() => {
+        let modal = new Modal(
+            document.getElementById("modalPelanggaran")
+        )
+        setModal(modal)
         getData()
     }, [])
 
     return (
         <div className="container-fluid">
+
+            {/** Compnent Toast */}
+            <div className="position-fixed top-0 end-0 p-3" style={{ zIndex: 11 }}>
+                <div className="toast bg-light" id="myToast">
+                    <div className="toast-header bg-info text-white">
+                        <strong>Message</strong>
+                    </div>
+                    <div className="toast-body">
+                        {message}
+                    </div>
+                </div>
+            </div>
+            {/** Compnent Toast */}
+
             <div className="card m-2">
                 <div className="card-header"
                     style={{ background: `cornflowerblue` }}>
@@ -50,7 +143,7 @@ export default function Pelanggaran() {
                         {pelanggaran.map(item => (
                             <li className="list-group-item" style={{ background: `mintcream` }} key={`key-${item.id_pelanggaran}`}>
                                 <div className="row">
-                                    <div className="col-3">
+                                    <div className="col-2">
                                         <small className="text-info">
                                             ID Pelanggaran
                                         </small>
@@ -62,16 +155,59 @@ export default function Pelanggaran() {
                                         </small>
                                         <h5>{item.nama_pelanggaran}</h5>
                                     </div>
-                                    <div className="col-3">
+                                    <div className="col-2">
                                         <small className="text-info">
                                             Point
                                         </small>
                                         <h5>{item.poin}</h5>
                                     </div>
+                                    <div className="col-2 mt-3">
+                                        {/** tombol Edit */}
+                                        <button className="btn btn-sm btn-outline-primary mx-1"
+                                            onClick={() => editData(item)}>
+                                            <span className="fa fa-edit"></span>
+                                        </button>
+                                        {/** tombol delete */}
+                                        <button className="btn btn-sm btn-danger mx-1">
+                                            <span className="fa fa-trash"></span>
+                                        </button>
+                                    </div>
                                 </div>
                             </li>
                         ))}
                     </ul>
+
+                    {/** Button Tambah */}
+                    <button className="btn btn-md btn-outline-success mt-3"
+                        onClick={() => tambahData()}>
+                        <span className="fa fa-plus"></span> Tambah Data
+                    </button>
+
+                    {/** buat modal yg isinya form utk data pelanggaran */}
+                    <div className="modal" id="modalPelanggaran">
+                        <div className="modal-dialog modal-md">
+                            <div className="modal-content">
+                                <div className="modal-header bg-info">
+                                    <h4 className="text-white">Form Pelanggaran</h4>
+                                </div>
+                                <div className="modal-body">
+                                    <form onSubmit={ev => saveData(ev)}>
+                                        Jenis Pelanggaran
+                                        <input type="text" className="form-control mb-2" required
+                                            onChange={e => setNamaPelanggaran(e.target.value)}
+                                            value={namaPelanggaran} />
+                                        Point
+                                        <input type="number" className="form-control mb-2" required
+                                            onChange={e => setPoin(e.target.value)}
+                                            value={poin} />
+                                        <button className="btn btn-sm btn-success mt-1">
+                                            <span className="fa fa-check"></span> Simpan
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
